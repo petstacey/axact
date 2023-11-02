975ebbfe5bb6195a51e68870681b1ebc6607f0c6
pso-dev <peter@petertsacey.com.au>
2023-11-02 11:19:09 +1100

    added api functionality

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ use std::sync::{Arc, Mutex};
 
 #[tokio::main]
 async fn main() {
-    let router: Router = Router::new().route("/", get(root_get)).with_state(AppState {
-        sys: Arc::new(Mutex::new(System::new())),
-    });
+    let router: Router = Router::new()
+        .route("/", get(root_get))
+        .route("/api/cpus", get(cpus_get)).with_state(AppState {
+            sys: Arc::new(Mutex::new(System::new())),
+        });
 
     let server = Server::bind(&"0.0.0.0:6969".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
@@ -22,7 +24,11 @@ struct AppState {
     sys: Arc<Mutex<System>>,
 }
 
-async fn root_get(State(state): State<AppState>) -> String {
+async fn root_get() -> &'static str {
+    "Hello World"
+}
+
+async fn cpus_get(State(state): State<AppState>) -> String {
     let mut s: String = String::new();
 
     let mut sys = state.sys.lock().unwrap();
